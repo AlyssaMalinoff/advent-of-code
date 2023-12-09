e38b0659aaab882f60abe424b046db415acee920
Alyssa Malinoff <alyssamalinoff@gmail.com>
2023-12-09 14:31:42 -0800

    Wow this was fun, so far. Logic checks out with sample input. Time to try the actual input

--- 2023/day-4-scratchcards/main.py
+++ 2023/day-4-scratchcards/main.py
@@ -10,10 +10,9 @@ Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11
 """
 
 
-def split_games():
+def split_games(input_text):
     # splitting the card games
     lines = input_text.strip().split("\n")
-
     cards = {}
 
     for line in lines:
@@ -22,7 +21,7 @@ def split_games():
         # card game number
         card_number = game_info[0].strip()
 
-        # splitting the numbers from the card game number
+        # splitting the winning/my_numbers from the card game number
         numbers = game_info[1].strip().split("|")
 
         winning_numbers = list(map(int, numbers[0].strip().split()))
@@ -33,11 +32,47 @@ def split_games():
             "my_numbers": game_numbers,
         }
 
-    for card, data in cards.items():
+    return cards
+
+    # for card, data in cards.items():
+    #     print(f"Card Game: {card}")
+    #     # print(f"Data keys: {data.keys()}")
+    #     print(f"Winning Numbers: {data.get('winning_numbers')}")
+    #     print(f"My Numbers: {data.get('my_numbers')}")
+
+
+def compare_numbers(cards_data):
+    game_point_total = {}
+
+    for card, data in cards_data.items():
+        winning_numbers = set(data["winning_numbers"])
+        my_numbers = set(data["my_numbers"])
+
+        # compares the winning numbers and the game numbers
+        matching_numbers = winning_numbers.intersection(my_numbers)
+
+        matches = len(matching_numbers)
+
+        points = 2 ** (matches - 1) if matches > 0 else 0
+
+        game_point_total[card] = points
+
+        overall_point_totals = sum(points for game, points in game_point_total.items())
+
         print(f"Card Game: {card}")
-        # print(f"Data keys: {data.keys()}")
-        print(f"Winning Numbers: {data.get('winning_numbers')}")
-        print(f"My Numbers: {data.get('my_numbers')}")
+        print(f"Winning Numbers: {winning_numbers}")
+        print(f"My Numbers: {my_numbers}")
+        # fixes the issue of Matching numbers displaying set()
+        print(
+            f"Matching Numbers: {'No Matching Numbers' if not matching_numbers else matching_numbers}"
+        )
+        print(f"Game Point Total: {points}")
+
+    print(f"Summary: {game_point_total}")
+    print(f"Part 1: {overall_point_totals}")
+
+    return overall_point_totals
 
 
-split_games()
+cards_data = split_games(input_text)
+game_points = compare_numbers(cards_data)
