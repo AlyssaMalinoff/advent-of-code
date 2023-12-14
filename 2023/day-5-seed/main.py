3ec6c367ca9c4f2b8580c411915c3278910f95a5
Alyssa Malinoff <alyssamalinoff@gmail.com>
2023-12-14 14:07:38 -0800

    finished day 5, thanks Cursor

--- 2023/day-5-seed/main.py
+++ 2023/day-5-seed/main.py
@@ -1,28 +1,31 @@
+inputs, *blocks = open("input.txt").read().split("\n\n")
 
-def map_seeds_through_all_mappings(input):
-    with open("input.txt", 'r') as file:
-        input = file.read()
+inputs = list(map(int, inputs.split(":")[1].split()))
 
-    seeds, *blocks = input.split("\n\n")
-    seeds = list(map(int, seeds.split(":")[1].split()))
+seed_ranges = []
 
-    for block in blocks:
-        seed_ranges = []
-        for line in block.splitlines()[1:]:
-            seed_ranges.append(list(map(int, line.split())))
-        mapped_seeds = []
-        for x in seeds:
-            for first, last, range_length in seed_ranges:
-                if last <= x < last + range_length:
-                    mapped_seeds.append(x - last + first)
-                    break
-            else:
-                mapped_seeds.append(x)
-        seeds = mapped_seeds
+for i in range(0, len(inputs), 2):
+    seed_ranges.append((inputs[i], inputs[i] + inputs[i + 1]))
 
-    return seeds
+for block in blocks:
+    block_ranges = []
+    for line in block.splitlines()[1:]:
+        block_ranges.append(list(map(int, line.split())))
+    new_seed_ranges = []
+    while len(seed_ranges) > 0:
+        seed_start, seed_end = seed_ranges.pop()
+        for block_start, range_start, range_length in block_ranges:
+            overlap_start = max(seed_start, range_start)
+            overlap_end = min(seed_end, range_start + range_length)
+            if overlap_start < overlap_end:
+                new_seed_ranges.append((overlap_start - range_start + block_start, overlap_end - range_start + block_start))
+                if overlap_start > seed_start:
+                    seed_ranges.append((seed_start, overlap_start))
+                if seed_end > overlap_end:
+                    seed_ranges.append((overlap_end, seed_end))
+                break
+        else:
+            new_seed_ranges.append((seed_start, seed_end))
+    seed_ranges = new_seed_ranges
 
-# Use the function
-final_seeds = map_seeds_through_all_mappings("input.txt")
-print(f"Final Seeds: {final_seeds}")
-print(f"Part 1: {min(final_seeds)}")
+print(min(seed_ranges)[0])
