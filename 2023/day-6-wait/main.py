4ef687e4b7a39b8e0fa9eff58eced471af3c1ebb
Alyssa Malinoff <alyssamalinoff@gmail.com>
2023-12-21 21:26:52 -0800

    had to do some weird file import method...BUT it works

--- 2023/day-6-wait/main.py
+++ 2023/day-6-wait/main.py
@@ -1,7 +1,15 @@
-with open("input.txt", "r") as file:
-        input_text = file.read()
+import os
 
-times, distances = [list(map(int, line.split(":")[1].split())) for line in open(input_text)]
+# Get the directory of the script
+script_dir = os.path.dirname(os.path.realpath(__file__))
+
+# Change the working directory to the script directory
+os.chdir(script_dir)
+
+with open("input.txt", "r") as file:
+    input_text = file.read().splitlines()
+
+times, distances = [list(map(int, line.split(":")[1].split())) for line in input_text if ":" in line]
 
 TotalRecord = 1
 
@@ -12,4 +20,4 @@ for time, distance in zip(times, distances):
             margin += 1
     TotalRecord *= margin
 
-print(TotalRecord)
+print(f"Part 1: {TotalRecord}")
