159869aa2fb06315f7d3ebd9c7852da1e019291f
Alyssa Malinoff <alyssamalinoff@gmail.com>
2023-12-21 21:29:54 -0800

    oh i found out why the file wouldnt import ops

--- 2023/day-6-wait/main.py
+++ 2023/day-6-wait/main.py
@@ -1,23 +1,15 @@
-import os
-
-# Get the directory of the script
-script_dir = os.path.dirname(os.path.realpath(__file__))
-
-# Change the working directory to the script directory
-os.chdir(script_dir)
-
 with open("input.txt", "r") as file:
     input_text = file.read().splitlines()
 
 times, distances = [list(map(int, line.split(":")[1].split())) for line in input_text if ":" in line]
 
-TotalRecord = 1
+total_record = 1
 
 for time, distance in zip(times, distances):
     margin = 0
-    for hold in range(time):
-        if hold * (time - hold) > distance:
+    for button_hold in range(time):
+        if button_hold * (time - button_hold) > distance:
             margin += 1
-    TotalRecord *= margin
+    total_record *= margin
 
-print(f"Part 1: {TotalRecord}")
+print(f"Part 1: {total_record}")
